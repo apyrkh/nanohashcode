9aa5fe0391f10ec01fcea606708ce552ac01fcfa
Aliaksandr Pyrkh <alexpyrkh@gmail.com>
2022-12-04 00:27:18 +0100

    Added size-limit test

--- test/index.test.js
+++ test/index.test.js
@@ -1,19 +1,24 @@
 import { test } from 'uvu'
 import { is } from 'uvu/assert'
 
-import { hashCode } from '../index.js'
+import { hashCode as hashCodeNode } from '../index.js'
+import { hashCode as hashCodeBrowser } from '../index.browser.js'
 
-test('hashCode returns 0 for undefined', () => {
-  is(hashCode(), 0)
-})
-test('hashCode returns 0 for null', () => {
-  is(hashCode(null), 0)
-})
-test('hashCode returns 0 for empty string', () => {
-  is(hashCode(''), 0)
-})
-test('hashCode returns 1794106052 for \'hello world\'', () => {
-  is(hashCode('hello world'), 1794106052)
-})
+for (let type of ['node', 'browser']) {
+  const hashCode = type === 'node' ? hashCodeNode : hashCodeBrowser
+
+  test(`${type} / hashCode returns 0 for undefined`, () => {
+    is(hashCode(), 0)
+  })
+  test(`${type} / hashCode returns 0 for null`, () => {
+    is(hashCode(null), 0)
+  })
+  test(`${type} / hashCode returns 0 for empty string`, () => {
+    is(hashCode(''), 0)
+  })
+  test(`${type} / hashCode returns 1794106052 for 'hello world'`, () => {
+    is(hashCode('hello world'), 1794106052)
+  })
+}
 
 test.run()
